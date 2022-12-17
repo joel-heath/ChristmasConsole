35535ea0fbf8ef931d090ac4bb8b724077793b41
Joel Heath <joelheath@outlook.com>
2022-12-17 12:23:31 +0000

    Add project files.

---
+++ Christmas/Outdoors.cs
@@ -0,0 +1,240 @@
+﻿using System.Drawing;
+
+namespace Christmas;
+internal partial class Program
+{
+    class Snow
+    {
+        public Point Location;
+        public ConsoleColor Color;
+        public char Character;
+        public int Velocity; // inverse (1/velocity)
+        public int Timer;
+
+        public Snow(Point location, ConsoleColor color, char character, int velocity)
+        {
+            this.Location = location;
+            this.Color = color;
+            this.Character = character;
+            this.Velocity = velocity;
+            this.Timer = 0;
+        }
+    }
+    class Block
+    {
+        public Point Location;
+        public ConsoleColor Color;
+        public char Character;
+
+        public Block(Point location, ConsoleColor color, char character)
+        {
+            this.Location = location;
+            this.Color = color;
+            this.Character = character;
+        }
+
+        public static explicit operator Block(Snow s) => new(s.Location, s.Color, s.Character);
+    }
+
+    static Snow MoveSnow(List<Block> map, Snow snow)
+    {
+        Point[] vectors = { (0, 1), (-1, 1), (1, 1) };
+
+        foreach (Point vector in vectors)
+        {
+            Snow newSnow = new(snow.Location + vector, snow.Color, snow.Character, snow.Velocity);
+
+            bool positionBlocked = false;
+            foreach (Block block in map)
+            {
+                if (block.Location == newSnow.Location)
+                    positionBlocked = true;
+            }
+
+            if (positionBlocked == true)
+                continue;
+
+            if (newSnow.Location.Y > Console.WindowHeight - 1 || newSnow.Location.X < 0 || newSnow.Location.X > Console.WindowWidth - 2)
+                continue;
+
+            return newSnow;
+        }
+
+        return snow;
+    }
+
+    static void DrawForegroundSnow(List<Block> map, List<Snow> snowflakes, List<Block> background, char[] symbols)
+    {
+        List<Snow> toRemove = new();
+
+        for (int i = 0; i < snowflakes.Count; i++)
+        {
+            Snow snow = snowflakes[i];
+            snow.Timer = ++snow.Timer % snow.Velocity;
+
+            if (snow.Timer == 0)
+            {
+                Snow newSnow = MoveSnow(map, snow);
+                if (newSnow.Location == snow.Location)
+                {
+                    map.Add((Block)snow);
+                    toRemove.Add(snow);
+                    continue;
+                }
+
+                var overwrite = background.Where(b => b.Location == snow.Location).DefaultIfEmpty(new Block(snow.Location, ConsoleColor.White, ' ')).First();
+                Console.SetCursorPosition(overwrite.Location.X, overwrite.Location.Y);
+                Console.ForegroundColor = overwrite.Color;
+                Console.Write(overwrite.Character);
+
+                Console.SetCursorPosition(newSnow.Location.X, newSnow.Location.Y);
+                Console.ForegroundColor = newSnow.Color;
+                newSnow.Character = RandItem(symbols.Where(s => s != snow.Character).ToArray());
+                Console.Write(newSnow.Character);
+
+                snowflakes[i] = newSnow;
+            }
+        }
+
+        foreach (Snow snow in toRemove)
+        {
+            snowflakes.Remove(snow);
+        }
+    }
+
+    static void DrawBackgroundSnow(List<Block> map, List<Snow> snowflakes, List<Block> foreground, char[] symbols)
+    {
+        List<int> toRemove = new();
+
+        for (int i = 0; i < snowflakes.Count; i++)
+        {
+            Snow snow = snowflakes[i];
+            snow.Timer = (snow.Timer + 1) % snow.Velocity;
+
+            if (snow.Timer == 0)
+            {
+                Snow newSnow = MoveSnow(map, snow);
+                if (newSnow.Location == snow.Location)
+                {
+                    map.Add((Block)snow);
+                    toRemove.Add(i);
+                    continue;
+                }
+
+                // overwrite old position if nothing in the way
+                if (!foreground.Where(b => b.Location == snow.Location).Any())
+                {
+                    Console.SetCursorPosition(snow.Location.X, snow.Location.Y);
+                    Console.Write(' ');
+                }
+
+                snowflakes[i] = newSnow;
+
+                // dont overrite foregorund, go behind it
+                if (foreground.Where(b => b.Location == newSnow.Location).Any()) continue;
+
+                Console.SetCursorPosition(newSnow.Location.X, newSnow.Location.Y);
+                Console.ForegroundColor = newSnow.Color;
+                newSnow.Character = RandItem(symbols.Where(s => s != snow.Character).ToArray());
+                Console.Write(newSnow.Character);
+            }
+        }
+
+        foreach (int index in toRemove)
+        {
+            snowflakes.RemoveAt(index);
+        }
+    }
+
+    static void DrawMap(List<Block> map)
+    {
+        for (int i = 0; i < map.Count; i++)
+        {
+            Console.SetCursorPosition(map[i].Location.X, map[i].Location.Y);
+            Console.ForegroundColor = map[i].Color;
+            Console.Write(map[i].Character);
+        }
+    }
+
+    static List<Block> CreatePresent(ConsoleColor[] colors)
+    {
+        char symbol = '#';
+        List<Block> present = new();
+        Random random = new();
+
+        // make sure lengths and widths are odd so there is a centre
+        int width = 2 * random.Next(1, 5) + 1;
+        int height = 2 * random.Next(1, 5) + 1;
+        int xPos = random.Next(0, Console.WindowWidth - width);
+        int yPos = random.Next(5, Console.WindowHeight - height);
+
+        ConsoleColor primary = RandItem(colors);
+        ConsoleColor secondary = RandItem(colors.Where(c => c != primary).ToArray());
+
+        for (int y = yPos; y < yPos + height; y++)
+        {
+            for (int x = xPos; x < xPos + width; x++)
+            {
+                if (y - yPos == height / 2 || x - xPos == width / 2)
+                {
+                    present.Add(new Block((x, y), secondary, symbol));
+                }
+                else
+                {
+                    present.Add(new Block((x, y), primary, symbol));
+                }
+            }
+        }
+
+        return present;
+    }
+
+    static void GoOutside()
+    {
+        Console.Clear();
+        Console.CursorVisible = false;
+        Random random = new();
+
+        ConsoleColor[] foregroundColors = new[] { ConsoleColor.Red, ConsoleColor.Green, ConsoleColor.Blue, ConsoleColor.Cyan, ConsoleColor.Yellow, ConsoleColor.Magenta };
+        ConsoleColor[] backgroundColors = new[] { ConsoleColor.DarkRed, ConsoleColor.DarkGreen, ConsoleColor.Blue, ConsoleColor.DarkCyan, ConsoleColor.DarkYellow, ConsoleColor.DarkMagenta };
+
+        List<Block> foreground = new();
+        for (int i = 0; i < random.Next(4, 9); i++)
+        {
+            foreground.AddRange(CreatePresent(foregroundColors));
+        }
+
+        List<Block> background = new();
+        for (int i = 0; i < random.Next(4, 9); i++)
+        {
+            background.AddRange(CreatePresent(backgroundColors));
+        }
+
+        List<Snow> foregroundSnow = new();
+        List<Snow> backgroundSnow = new();
+
+        DrawMap(background);
+        DrawMap(foreground);
+
+        char[] snowflakes = new[] { '+', 'x', '*', '❄', '❅', '❆' };
+        //char[] snowflakes = new[] { '+', 'x', '*' };
+        ConsoleColor[] foregroundSnowColors = new[] { ConsoleColor.White, ConsoleColor.Gray };
+
+        while (!Console.KeyAvailable || Console.ReadKey(true).Key != ConsoleKey.Escape)
+        {
+            // add new snow
+            if (foregroundSnow.Count < 8 && random.NextDouble() > 0.7)
+                foregroundSnow.Add(new((random.Next(0, Console.WindowWidth), 0), RandItem(foregroundSnowColors), RandItem(snowflakes), random.Next(1, 4)));
+
+            if (backgroundSnow.Count < 8 && random.NextDouble() > 0.7)
+                backgroundSnow.Add(new((random.Next(0, Console.WindowWidth), 0), ConsoleColor.DarkGray, RandItem(snowflakes), random.Next(4, 10)));
+
+            Thread.Sleep(50);
+
+            DrawBackgroundSnow(background, backgroundSnow, foreground, snowflakes);
+            DrawForegroundSnow(foreground, foregroundSnow, background, snowflakes);
+        }
+
+        ConsoleWipe();
+    }
+}
---
+++ Christmas/Program.cs
@@ -0,0 +1,308 @@
+﻿namespace Christmas;
+internal partial class Program
+{
+    struct Point
+    {
+        public int X, Y;
+
+        public Point(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+
+        public static implicit operator Point((int, int) tuple) => new(tuple.Item1, tuple.Item2);
+
+        public static Point operator +(Point a, Point b) => new(a.X + b.X, a.Y + b.Y);
+
+        public static bool operator ==(Point a, Point b) => a.X == b.X && a.Y == b.Y;
+        public static bool operator !=(Point a, Point b) => !(a == b);
+
+        public override bool Equals(object? obj) => obj is Point p && p.X.Equals(X) && p.Y.Equals(Y);
+        public override int GetHashCode() => HashCode.Combine(X, Y);
+    }
+
+    static T RandItem<T>(T[] array) => array[Random.Shared.Next(array.Length)];
+
+    static void ConsoleWipe()
+    {
+        Console.SetCursorPosition(0, 0);
+        for (int r = 0; r < Console.WindowHeight; r++)
+        {
+            if (r % 2 == 0) // forawrds
+            {
+                for (int c = 0; c < Console.WindowWidth; c++)
+                {
+                    Console.Write(' ');
+                }
+                try { Console.CursorTop++; }
+                catch { }
+            }
+            else // backwards
+            {
+                for (int c = Console.WindowWidth - 1; c >= 0; c--)
+                {
+                    Console.Write(' ');
+                    Console.CursorLeft = c;
+                }
+                try { Console.CursorTop++; }
+                catch { }
+            }
+        }
+    }
+
+    /*
+    static int Choose(string[] options, bool escapable = true)
+    {
+        //ClearKeyBuffer();
+        int choice = 0;
+        int indent = (Console.WindowWidth / 2) - (options.Sum(o => o.Length + 10) / 2);
+        int xIndent = indent;
+        int yIndent = Console.WindowHeight - (3 + 3);
+        bool chosen = false;
+        while (!chosen)
+        {
+            Console.CursorVisible = false;
+            xIndent = indent;
+
+            // write all options with current selected highlighted
+            for (int i = 0; i < options.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.SetCursorPosition(xIndent, yIndent);
+                Console.WriteLine(new String('-', options[i].Length + 4));
+                Console.SetCursorPosition(xIndent, yIndent + 1);
+                Console.Write("| ");
+                if (choice == i)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                }
+                else
+                {
+                    //Console.ForegroundColor = options[i].Color;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                //Console.Write(options[i].Contents);
+                Console.Write(options[i]);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+
+                Console.WriteLine(" |");
+                Console.SetCursorPosition(xIndent, yIndent + 2);
+                Console.Write(new String('-', options[i].Length + 4));
+
+                xIndent += options[i].Length + 10;
+            }
+
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.RightArrow:
+                    if (choice < options.Length - 1)
+                    {
+                        //AudioPlaybackEngine.Instance.PlaySound(menuBleep);
+                        choice++;
+                        //MainConsole.Refresh();
+                    }; break;
+                case ConsoleKey.LeftArrow:
+                    if (choice > 0)
+                    {
+                        choice--;
+                    }
+                    break;
+                case ConsoleKey.Spacebar:
+                case ConsoleKey.Enter: chosen = true; break;
+                case ConsoleKey.Escape:
+                    if (escapable)
+                    {
+                        choice = -1;
+                        chosen = true;
+                    }
+                    break;
+            }
+
+            Console.CursorVisible = true;
+        }
+
+        return choice;
+    }
+    */
+
+    static int Choose(string[] options, bool escapable = true)
+    {
+        //ClearKeyBuffer();
+        int choice = 0;
+        int indent = (Console.WindowWidth / 2) - (options.Sum(o => o.Length + 10) / 2);
+        int xIndent = indent;
+        int yIndent = Console.WindowHeight - (2 + 3);
+        bool chosen = false;
+
+        int textBoxLength = options.Max(o => o.Length);
+
+        while (!chosen)
+        {
+            Console.CursorVisible = false;
+            xIndent = indent;
+
+            // write all options with current selected highlighted
+            for (int i = 0; i < options.Length; i++)
+            {
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.SetCursorPosition(xIndent, yIndent);
+                Console.WriteLine(new String('-', textBoxLength + 4));
+                int centering = (textBoxLength - options[i].Length+1) / 2;
+                Console.SetCursorPosition(xIndent, yIndent + 1);
+                Console.Write("| " + new string(' ', centering));
+                if (choice == i)
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.BackgroundColor = ConsoleColor.DarkGray;
+                }
+                else
+                {
+                    //Console.ForegroundColor = options[i].Color;
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                //Console.Write(options[i].Contents);
+                Console.Write(options[i]);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.BackgroundColor = ConsoleColor.Black;
+
+                Console.CursorLeft = xIndent + textBoxLength + 2;
+                Console.Write(" |");
+                Console.SetCursorPosition(xIndent, yIndent + 2);
+                Console.Write(new String('-', textBoxLength + 4));
+
+                xIndent += options[i].Length + 10;
+            }
+
+            switch (Console.ReadKey(true).Key)
+            {
+                case ConsoleKey.RightArrow:
+                    if (choice < options.Length - 1)
+                    {
+                        //AudioPlaybackEngine.Instance.PlaySound(menuBleep);
+                        choice++;
+                        //MainConsole.Refresh();
+                    }; break;
+                case ConsoleKey.LeftArrow:
+                    if (choice > 0)
+                    {
+                        choice--;
+                    }
+                    break;
+                case ConsoleKey.Spacebar:
+                case ConsoleKey.Enter: chosen = true; break;
+                case ConsoleKey.Escape:
+                    if (escapable)
+                    {
+                        choice = -1;
+                        chosen = true;
+                    }
+                    break;
+            }
+
+            Console.CursorVisible = true;
+        }
+
+        return choice;
+    }
+
+    static void ViewTree()
+    {
+        ConsoleColor[] TreeColors = { ConsoleColor.Green, ConsoleColor.DarkGreen };
+        ConsoleColor[] OrnamentColors = { ConsoleColor.Red, ConsoleColor.Blue, ConsoleColor.Yellow, ConsoleColor.Magenta, ConsoleColor.Cyan };
+
+        int rows = 18;
+        int totalChars = rows * 2 - 1;
+        int stars = 3;
+        int spaces = (totalChars - stars) / 2;
+        int centering = (Console.WindowWidth - totalChars - 2) / 2;
+
+        try
+        {
+            Console.SetCursorPosition((Console.WindowWidth - 1) / 2 - 1, 1);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write('*');
+            Console.CursorTop++;
+
+            for (int row = 0; row < rows; row++)
+            {
+                Console.CursorLeft = centering + spaces;
+                for (int i = 0; i < stars; i++)
+                {
+                    if (Random.Shared.NextDouble() > 0.8)
+                    {
+                        Console.ForegroundColor = RandItem(OrnamentColors);
+                        Console.Write('o');
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = RandItem(TreeColors);
+                        Console.Write('*');
+                    }
+                }
+
+                Console.CursorTop++;
+                spaces--;
+                stars += 2;
+            }
+
+            Console.CursorLeft = Console.WindowWidth / 2 - 3;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("###"); Console.CursorTop++; Console.CursorLeft -= 5;
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write("# ");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("### ");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.Write('#');
+            Console.CursorTop++; Console.CursorLeft -= 7;
+            Console.Write("#######"); Console.CursorTop++; Console.CursorLeft -= 7;
+            Console.Write("#######");
+        }
+        catch (System.ArgumentOutOfRangeException)
+        {
+            // user pressed arrow key while tree was being drawn
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            return;
+        }
+    }
+
+    static void TickTimer(object? state)
+    {
+        ViewTree();
+        Thread.Sleep(2000);
+    }
+
+    static void Main()
+    {
+        //Console.OutputEncoding = System.Text.Encoding.UTF8;
+        string[] options = new string[] { "Look Outside", "Open A Present" };
+        Timer timer = new(new TimerCallback(TickTimer), null, 1000, 1000);
+
+        ViewTree();
+        bool @continue = true;
+        while (@continue)
+        {
+            switch (Choose(options, true))
+            {
+                case 0:
+                    timer.Change(Timeout.Infinite, Timeout.Infinite);
+                    GoOutside();
+                    ViewTree();
+                    timer.Change(1000, 1000);
+                    break;
+                case 1:
+                    throw new NotImplementedException();
+                    //break;
+                case -1:
+                    @continue = false;
+                    break;
+            }
+        }
+    }
+}
